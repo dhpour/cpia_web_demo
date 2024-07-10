4735b70994bcae792b13d9b269142a7465a71da3
lingwndr <chandracar@gmail.com>
2024-07-10 23:40:57 +0330

    style: fixd favicon configs

--- inflapp/urls.py
+++ inflapp/urls.py
@@ -24,5 +24,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     #path('admin/', admin.site.urls),
     url(r'^', include('farsi_infl.urls')),   # To make post app available at /
-    path("images/favicon.png", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.png"))),
+    path("favicon.png", RedirectView.as_view(url=staticfiles_storage.url("images/favicon.png"))),
 ]
