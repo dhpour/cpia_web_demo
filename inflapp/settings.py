32a2e4e5acdc604e5e4eaa7028baf01d1bad258c
lingwndr <chandracar@gmail.com>
2024-07-10 21:21:19 +0330

    build: removed load_dotenv and import

--- inflapp/settings.py
+++ inflapp/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
